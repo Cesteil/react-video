98fdc1bd5de8911853b1b1f72f8cbf729e83977c
Casey Schulkins <casey.schulkins.u2x5@statefarm.com>
2023-05-23 12:02:22 -0500

    created reducer, defined constants, and initial setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import NavBar from './NavBar';
 import VideoPlayer from './VideoPlayer';
 import VideoList from './VideoList';
 import videoData from './videoData';
+import {useReducer, useEffect} from "react";
+
+
 
 function App() {
+  const videos = [];
+  const selectedVideo = '';
+  const [state, dispatch] = 
+    useReducer(reducer, selectedVideo);
+  
+  function reducer(prevVideo, action) {
+    switch (action.type) {
+      case "SET_VIDEO":
+        return {videoId: action.payload}
+      default:
+        return prevVideo;
+    }
+  }
+  
+  // function handleVideoClick() {
+  //   dispatch({type: ''});
+  // }
 
   function fetchVideos() {
     return videoData;
