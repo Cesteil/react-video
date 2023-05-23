e28b1d02ded2bbb862cadff0c7f3f4bba2ada5f9
Connor Steiling <connor.steiling.xaa6@statefarm.com>
2023-05-23 16:22:55 -0500

    Stretch goal functionality working

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function reducer(state, action) {
      return {...state, videos: action.payload};
     case "SET_SELECTED_VIDEO":
       return {...state, selectedVideo: action.payload}
+    case "SET_SEARCHED_VIDEO":
+      let searchString = action.payload;
+      let videoArrayCopy = state.videos;
+      videoArrayCopy = videoArrayCopy.filter(x => x.snippet.title.includes(searchString))
+      return {...state, videos: videoArrayCopy}
     default:
       return state;
   }
@@ -18,15 +23,14 @@ function reducer(state, action) {
 
 const initialState = {
   videos:[],
-  selectedVideo:null
+  selectedVideo:null,
+  searchedVideo:null
 }
 
 function App() {
   const [state, dispatch] = 
     useReducer(reducer, initialState);
   
-  console.log(state);
-
   useEffect(() => {
     const action = {
       type: "SET_VIDEOS",
@@ -41,7 +45,7 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
+      <NavBar dispatch={dispatch}/>
       <section className="content-container">
         <VideoPlayer selectedVideo={state.selectedVideo}/>
         <VideoList videos={state.videos} dispatch={dispatch}/>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,19 @@
 
-function NavBar() {
+function NavBar({dispatch}) {
+  
+  function searchVideo(e) {
+    e.preventDefault();
+    const action = {
+      type: "SET_SEARCHED_VIDEO",
+      payload: e.target.search.value
+    }
+    dispatch(action);
+  }
+
   return (
     <div className="app-nav">
       <h1>SF Video</h1>
-      <form>
+      <form onSubmit={searchVideo}>
         <label htmlFor="search">
           Search 
         </label>
