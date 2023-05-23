ae3851e35a9c0dd16e4ff9289a3174bbdaf46c0d
Connor Steiling <connor.steiling.xaa6@statefarm.com>
2023-05-23 15:45:56 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import NavBar from './NavBar';
 import VideoPlayer from './VideoPlayer';
 import VideoList from './VideoList';
 import videoData from './videoData';
+import {useReducer, useEffect} from 'react';
 
 function App() {
+
+  const videos = [];
+  const selectedVideo = '';
+  const [state,dispatch] = useReducer(reducer,selectedVideo);
+
+  function reducer(state,action){
+    switch (action.type){
+      case 'SET_VIDEO':
+        return state.selectedVideo;
+      default :
+        return console.log('failure')
+    }
+  }
 
   function fetchVideos() {
     return videoData;
@@ -15,7 +29,7 @@ function App() {
       <NavBar />
       <section className="content-container">
         <VideoPlayer selectedVideo={null}/>
-        <VideoList videos={fetchVideos()}/>
+        <VideoList videos={fetchVideos()} onClick={() => dispatch({type: 'SET_VIDEO'})}/>
       </section>
     </div>
   );
